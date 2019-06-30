4345a7a10ae5675fc537b785b8e5646603df94eb
Amy Merrill <amous.fullstack@gmail.com>
2019-06-29 18:14:16 -0700

    fixed file path

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -9,7 +9,15 @@ function FriendCard(props) {
       onClick={() => props.handleClick(props.id)}
     >
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img
+          alt={props.name}
+          src={props.image}
+          style={
+            {
+              // backgroundImage: `url('/assets/characters/${props.image}')`
+            }
+          }
+        />
       </div>
     </div>
   );
